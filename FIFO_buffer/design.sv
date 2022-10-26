6d63f61c7ea28ac9abec25912fa5722ff5566775
Daniel Giftson E <76448256+Dany2002-hub@users.noreply.github.com>
2022-10-26 19:09:34 +0530

    Update design.sv

--- FIFO_buffer/design.sv
+++ FIFO_buffer/design.sv
@@ -1,24 +1,24 @@
-module Fifo_buffer#(parameter n=4)
+module Fifo_buffer#(parameter k=4)
   (clk, RD, WR, data, out, emp, full);
   
   input clk, RD, WR;
   input [15:0] data;
   output reg [15:0] out;
   output emp, full;
-  reg [15:0] buffer [0:n];
+  reg [15:0] buffer [0:k];
   reg [2:0] read_count = 0;
   reg [2:0] write_count = 0;
   reg [2:0] count = 0;
   
   assign emp = (count == 0)? 1'b1:1'b0;
-  assign full = (count == n+1)? 1'b1:1'b0;
+  assign full = (count == k+1)? 1'b1:1'b0;
   
   always@(posedge clk)
     begin
       if(WR == 1'b1 && RD == 1'b0)
         begin
           read_count = 0;
-          if(count< n+1)
+          if(count< k+1)
             begin
               buffer[write_count] = data;
               write_count = write_count + 1;
